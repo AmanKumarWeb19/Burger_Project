740be6e6e5caa227131b3710f63c87a95bacc78d
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-09-29 10:06:31 +0530

    add color in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: "#010a5e",
+        primaryColorLight: "#010d78",
+        secondaryColor: "#FFCC00",
+        paragraphColor: "#c0c0c0",
+        whiteColor: "#fff",
+        blackColor: "#000",
+        greenColor: "#007936",
+        redColor: "cc3433",
+        darkColor: "#000",
+        dorkColorLight: "#171717",
+      },
+    },
   },
   plugins: [],
 };
