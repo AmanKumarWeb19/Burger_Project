be80091b7d1d26d6b0e512b2df0bb3ef6f85ba16
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-09-30 23:40:52 +0530

    menu section complete

--- src/main.js
+++ src/main.js
@@ -16,3 +16,45 @@ closeIcon.addEventListener("click", () => {
 hamburger.addEventListener("click", () => {
   navMenu.classList.remove("hidden");
 });
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Tabs~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+const tabs = document.querySelectorAll(".tabs_wrap ul li");
+
+const all = document.querySelectorAll(".item_wrap");
+const foods = document.querySelectorAll(".food");
+const snacks = document.querySelectorAll(".snack");
+const beverages = document.querySelectorAll(".beverage");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+
+    const tabVal = tab.getAttribute("data-tabs");
+
+    all.forEach((item) => {
+      item.style.display = "none";
+    });
+
+    if (tabVal == "food") {
+      foods.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else if (tabVal == "snack") {
+      snacks.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else if (tabVal == "beverage") {
+      beverages.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else {
+      all.forEach((item) => {
+        item.style.display = "block";
+      });
+    }
+  });
+});
