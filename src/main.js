bffd05215f2163a84b371aca33062a9402be572f
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-10-02 10:49:44 +0530

    dark mode section complete

--- src/main.js
+++ src/main.js
@@ -59,6 +59,37 @@ tabs.forEach((tab) => {
   });
 });
 
+// .........................................................Dark THEME........................................................
+
+const html = document.querySelector("html");
+const themeBtn = document.getElementById("theme-toggle");
+
+if (localStorage.getItem("mode") == "dark") {
+  darkMode();
+} else {
+  lightMode();
+}
+
+themeBtn.addEventListener("click", (e) => {
+  if (localStorage.getItem("mode") == "light") {
+    darkMode();
+  } else {
+    lightMode();
+  }
+});
+
+function darkMode() {
+  html.classList.add("dark");
+  themeBtn.classList.replace("ri-moon-line", "ri-sun-line");
+  localStorage.setItem("mode", "dark");
+}
+
+function lightMode() {
+  html.classList.remove("dark");
+  themeBtn.classList.replace("ri-sun-line", "ri-moon-line");
+  localStorage.setItem("mode", "light");
+}
+
 // ..........................................scroll up function..........................................................
 
 const scrollUp = () => {
